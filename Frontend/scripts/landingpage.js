f3e6dcd04d336ec757a88859e62e046ba82fea85
xinying00663 <limxinying060327@gmail.com>
2025-09-04 11:01:26 +0800

    Changed

---
+++ Frontend/scripts/landingpage.js
@@ -0,0 +1,13 @@
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("active");
+        } else {
+          entry.target.classList.remove("active");
+        }
+      });
+    }, { threshold: 0.5 });
+
+    sections.forEach(section => observer.observe(section));
