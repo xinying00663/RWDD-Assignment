8bd96c20aa4588cd493d17f2dd7a9ab49fe869dd
xinying00663 <limxinying060327@gmail.com>
2025-09-09 17:36:51 +0800

    ...

---
+++ Frontend/scripts/sidebar.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("sidebar.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("sidebar").innerHTML = data;
+    })
+    .catch(error => console.error("Error loading sidebar:", error));
+});
