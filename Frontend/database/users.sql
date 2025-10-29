67cac612e86e15dcef816bb5749ace7e78dc767f
xinying00663 <limxinying060327@gmail.com>
2025-10-29 17:09:17 +0800

    .

--- Frontend/database/users.sql
+++ Frontend/database/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2025 at 08:43 AM
+-- Generation Time: Oct 29, 2025 at 10:06 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,24 +29,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `UserID` int(11) NOT NULL,
-  `Username` varchar(200) DEFAULT NULL,
-  `Full_Name` varchar(200) DEFAULT NULL,
-  `Gender` enum('Male','Female','Others','') DEFAULT NULL,
-  `Email` varchar(200) NOT NULL,
-  `Password` varchar(200) NOT NULL,
-  `Phone_Number` varchar(200) DEFAULT NULL,
-  `City_Or_Neighbourhood` varchar(200) DEFAULT NULL,
-  `Additional_info` varchar(200) DEFAULT NULL,
-  `Join_date` datetime DEFAULT NULL,
-  `Last_login` datetime DEFAULT NULL
+  `Username` varchar(455) DEFAULT NULL,
+  `Full_Name` varchar(455) DEFAULT NULL,
+  `Gender` enum('Male','Female','Others') DEFAULT NULL,
+  `Email` varchar(455) NOT NULL,
+  `Password` varchar(455) NOT NULL,
+  `Role` enum('user','admin') NOT NULL DEFAULT 'user',
+  `Phone_Number` varchar(455) DEFAULT NULL,
+  `City_Or_Neighbourhood` varchar(455) DEFAULT NULL,
+  `Additional_info` varchar(455) DEFAULT NULL,
+  `Join_date` date DEFAULT NULL,
+  `Last_login` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
-
-INSERT INTO `users` (`UserID`, `Username`, `Full_Name`, `Gender`, `Email`, `Password`, `Phone_Number`, `City_Or_Neighbourhood`, `Additional_info`, `Join_date`, `Last_login`) VALUES
-(3, NULL, NULL, NULL, 'jingtong0619@gmail.com', '$2y$10$4Z8jgEENvraZPVyShKC9We6QFNcnXfSxwXyeK3zjUFrAGZF.4l6jO', NULL, NULL, NULL, '2025-10-14 07:49:00', '2025-10-14 07:49:00');
+c:\Users\LIM\Downloads\password_resets.sql
 
 --
 -- Indexes for dumped tables
@@ -66,7 +65,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
